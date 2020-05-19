eb77b0dcc3cd9b4580f03a098721c1edb2689148
Denis <dnsmarinov23@gmail.com>
2020-05-19 17:49:31 +0300

    Section 7

---
+++ src/containers/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+import classes from './App.css'
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 1, name: 'Denis', age: 27 },
+      { id: 2, name: 'Oki', age: 24 },
+      { id: 3, name: 'Jiji', age: 26 },
+      { id: 4, name: 'Huy4o', age: 12 }
+    ],
+    showPersons: false
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  }
+
+  deletePersonHandler = (index) => {
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+
+    this.setState({ persons });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow })
+  }
+
+  render() {
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = <Persons
+        persons={this.state.persons}
+        clicked={this.deletePersonHandler}
+        changed={this.nameChangedHandler} />;
+    }
+
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
+
+        {persons}
+      </div>
+    );
+  }
+};
+
+export default App;
